5794a8ed2b23d689782fea285d47a12cb9cef8a6
Fakepng <krit0kasemtewin@gmail.com>
2022-06-25 02:27:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,82 +1,94 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const cors = require('cors');
-const userDB = require('./models/user');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const userDB = require("./models/user");
 const app = express();
-const middleware = require('./middleware');
+const middleware = require("./middleware");
 
 const PORT = process.env.PORT || 5050;
-const CLIENT = process.env.CLIENT || '*';
+const CLIENT = process.env.CLIENT || "*";
 const DATABASE = process.env.DATABASE;
 const JWT_SECRET = process.env.JWT_SECRET;
 
-const corsOptions ={
-    origin: CLIENT, 
-    credentials:true,
-    optionSuccessStatus:200
-}
+const corsOptions = {
+  origin: CLIENT,
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
 app.use(bodyParser.json());
-
 app.use(cors(corsOptions));
 
+(async () => {
+  try {
+    await mongoose.connect(DATABASE, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
+    console.log("Connected to MongoDB 💽");
+  } catch (error) {
+    console.log(error);
+  }
+})();
 
-app.get('/', (req, res) => {
-    res.json({ message: "welecome to fakepng api backend", contact: "contact@fakepng.com" });
+app.get("/", (req, res) => {
+  res.json({
+    message: "welecome to fakepng api backend",
+    contact: "contact@fakepng.com",
+  });
 });
 
-app.post('/create', async (req, res) => {
-    try {
-        const { username } = req.body;
-        const oldUser = userDB.findOne({ username });
-        if (oldUser.username === username) return res.status(409).send("User already exists")
-        const token = jwt.sign({
-            username,
-            role: 'user'
-        }, JWT_SECRET, {
-            expiresIn: '1h'
-        });
-        await userDB.create({ username, token })
-        return res.json({ token });
-    } catch (err) {
-        console.error(err);
-    }
+app.post("/create", async (req, res) => {
+  try {
+    const { username } = req.body;
+    const oldUser = userDB.findOne({ username });
+    if (oldUser.username === username)
+      return res.status(409).send("User already exists");
+    const token = jwt.sign(
+      {
+        username,
+        role: "user",
+      },
+      JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+    await userDB.create({ username, token });
+    return res.json({ token });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-app.post('/login', async (req, res) => {
-    try {
-        const { username } = req.body;
-        const token = jwt.sign({
-            username,
-            role: 'user'
-        }, JWT_SECRET, {
-            expiresIn: '1h'
-        });
-        await userDB.updateOne({ username }, { token });
-        res.json({ token });
-    } catch (err) {
-        console.error(err);
-    }
+app.post("/login", async (req, res) => {
+  try {
+    const { username } = req.body;
+    const token = jwt.sign(
+      {
+        username,
+        role: "user",
+      },
+      JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+    await userDB.updateOne({ username }, { token });
+    res.json({ token });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-
-app.get('/api', middleware, async (req, res) => {
-    res.json({ message: "welcome user", token: req.token });
+app.get("/api", middleware, async (req, res) => {
+  res.json({ message: "welcome user", token: req.token });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
-
-mongoose.connect(DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log("Connected to Database 💽");
-}).catch((err) => {
-    console.log(err)
+  console.log(`Server is listening on port ${PORT}`);
 });
